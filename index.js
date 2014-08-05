3b2541c4bb8935010ab92c000d10db427131eee4
popomore <sakura9515@gmail.com>
2014-08-05 23:12:57 +0800

    client.publish

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.config = require('./lib/config');
+exports.publish = require('./lib/publish');
+// exports.unpublish = require('./lib/unpublish');
+// exports.login = require('./lib/login');
+// exports.install = require('./lib/install');
+// exports.info = require('./lib/info');
+// exports.search = require('./lib/search');
