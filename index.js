e370c93e3d0b58bf9fc133d73b87e5bedafccb78
popomore <sakura9515@gmail.com>
2014-08-06 17:41:40 +0800

    add search

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ exports.unpublish = require('./lib/unpublish');
 // exports.login = require('./lib/login');
 // exports.install = require('./lib/install');
 exports.info = require('./lib/info');
-// exports.search = require('./lib/search');
+exports.search = require('./lib/search');
