eccd8cd6429bc6d651f2fdb8077950ffc9bd81f4
popomore <sakura9515@gmail.com>
2014-08-06 18:26:03 +0800

    add login

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 exports.config = require('./lib/config');
 exports.publish = require('./lib/publish');
 exports.unpublish = require('./lib/unpublish');
-// exports.login = require('./lib/login');
+exports.login = require('./lib/login');
 // exports.install = require('./lib/install');
 exports.info = require('./lib/info');
 exports.search = require('./lib/search');
