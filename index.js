5abc6a982ea4387cb984b6ea7c8cfee54f242ed9
popomore <sakura9515@gmail.com>
2014-08-06 17:22:54 +0800

    add info

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ exports.publish = require('./lib/publish');
 exports.unpublish = require('./lib/unpublish');
 // exports.login = require('./lib/login');
 // exports.install = require('./lib/install');
-// exports.info = require('./lib/info');
+exports.info = require('./lib/info');
 // exports.search = require('./lib/search');
